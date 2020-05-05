a2b7a322b2cd0d4d242ca7f25f0b50513948ced1
Eugene Cormier <eugenecormier@gmail.com>
2020-05-04 23:34:56 -0300

    created a subframe for settings which gets destroyed and repopulated upon device change

--- webcamsettings.py
+++ webcamsettings.py
@@ -28,13 +28,26 @@ def createwebcamdevlist():
 ###################################################
 root = Tk()
 root.title("Webcam Settings")
+
 mainframe = ttk.Frame(root, padding="0 0 0 0")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+mainframe.grid(row=0, column=0, sticky=(N, W, E, S))
+
+subframe = ttk.Frame(root, padding="0 0 0 0")
+subframe.grid(row=1, column=0, sticky=(W, E, S))
+
 webcamdevlist = createwebcamdevlist()
 
 # watch for setting changes
 def callback(*args):
-    print("variable changed!")
+    for i in subframe.winfo_children():
+        i.destroy()
+    v4l2output = subprocess.check_output(['v4l2-ctl', '-d', devicemenuVar.get(), '--list-ctrls']).strip().decode().splitlines()
+    deviceSettings = [i.split()[0] for i in v4l2output]
+    rowvar = 0
+    for i in deviceSettings:
+        globals()[i + 'Label'] = Label(subframe, text=i.capitalize().replace('_', ' '))
+        globals()[i + 'Label'].grid(row=rowvar, column=0, sticky=(W) ,padx=xpadding, pady=ypadding)
+        rowvar = rowvar + 1
 
 # device dropdown menu
 rowvar = 0
@@ -45,7 +58,10 @@ devicemenuVar.set(webcamdevlist[0])
 devicemenuMenu = OptionMenu(mainframe, devicemenuVar, *webcamdevlist)
 devicemenuMenu.grid(row=rowvar, column=1, sticky=W)
 devicemenuVar.trace('w', callback)
-rowvar = rowvar + 1
+
+
+# populate frame with settings
+callback()
 
 ###################################################
 # Main Program
